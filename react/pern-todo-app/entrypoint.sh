73fe63aee3bf2bb7efb20c78fcc8aca1f6c7f385
Nexlayer Deploy <nexlayer-deploy@example.com>
2025-10-03 12:19:08 -0400

    Update nginx configuration for Nexlayer pod networking

--- react/pern-todo-app/entrypoint.sh
+++ react/pern-todo-app/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Replace the environment variable in the Nginx configuration file
-envsubst '${EXPRESS_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+# Copy the Nginx configuration (no substitution needed for Nexlayer)
+cp /etc/nginx/conf.d/default.conf.template /etc/nginx/conf.d/default.conf
 
 # Start Nginx
 nginx -g 'daemon off;'
